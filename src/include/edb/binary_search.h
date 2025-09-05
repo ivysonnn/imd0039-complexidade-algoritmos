51f73a54a344df872d54be29ba835d3995abef06
Ivyson Lucas <ivysonlucas27@hotmail.com>
2025-09-05 20:48:09 -0300

    feat: binary search and quicksort header files

---
+++ src/include/edb/binary_search.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+namespace edb 
+{
+  namespace search
+  {
+    int binary_search(std::vector<int> arr, int key);
+  } 
+}
---
+++ src/include/edb/quicksort.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+namespace edb
+{
+  namespace sort
+  {
+    void quicksort(std::vector<int> arr);
+  }
+}
