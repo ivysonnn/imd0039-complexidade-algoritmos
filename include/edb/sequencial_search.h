ad833feb1d16c2a4337e5394d07a7e414454f08e
Chistian <chistian.silva.017@ufrn.edu.br>
2025-09-09 11:39:46 -0300

    added sequencial_search

---
+++ include/edb/sequencial_search.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+namespace edb 
+{
+    namespace search
+    {
+        int sequencial_search(const std::vector<int>& arr, int key);
+    } 
+}
