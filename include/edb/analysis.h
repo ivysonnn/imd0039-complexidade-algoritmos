d222013eab75519237bfa58b06ff823e643c4fc8
Ivyson Lucas <ivysonlucas27@hotmail.com>
2025-09-15 18:03:53 -0300

    feat: analyzer for search algorithms

--- include/edb/analysis.h
+++ include/edb/analysis.h
@@ -1,10 +1,31 @@
 #pragma once
 
 #include <iostream>
+#include <chrono>
 #include <vector>
+#include <algorithm>
+#include <random>
 #include <fstream>
+#include <filesystem>
+#include <functional>
+
+#include <edb/binary_search.h>
+#include <edb/sequencial_search.h>
+#include <edb/quicksort.h>
+#include <edb/bubblesort.h>
+
 
 namespace edb
 {
+    enum class algorithm_type
+    {
+        BUBBLE,
+        QUICK,
+        BINARY,
+        LINEAR,
+    };
+
     void run_analysis(std::string& algorithm_name);
+    void search_analyzer(algorithm_type t,const std::string& filename, const std::function<int(std::vector<int>&, int)>& search_algorithm);
+    std::vector<int> vec_setup(int n);
 }
