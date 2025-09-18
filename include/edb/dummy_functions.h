29a8e679ca79fb1eea91c5a045da736ecb598088
ChistianSilva <chistiansilva05@gmail.com>
2025-09-18 11:32:14 -0300

    feat: added function to generate desired complexity

---
+++ include/edb/dummy_functions.h
@@ -0,0 +1,18 @@
+#ifndef EDB_DUMMY_FUNCTIONS_H
+#define EDB_DUMMY_FUNCTIONS_H
+
+#include <vector>
+
+namespace edb {
+    namespace analysis {
+
+        void dummy_log_n(long long n);
+        void dummy_n(long long n);
+        void dummy_n_log_n(long long n);
+        void dummy_n_2(long long n);
+        void dummy_n_3(long long n);
+
+    }
+}
+
+#endif
